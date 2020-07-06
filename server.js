82e08f36d69dd01db51e9cb22cfcca5b1c58216b
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-06 17:36:25 -0400

    added view routes for the htmls pages on server

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ app.use(express.json());
 
 //Routes:
 app.get("/", (req, res) => {
-    res.send("Hello world")
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
 
 
 
