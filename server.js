6c4a34d92a3e6b54cc88fb2e5ef193c21b2279b0
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-06 18:19:45 -0400

    added static method

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
-//Routes:
+
+
+//View Routes:
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
+//Api Routes
+app.post("/notes")
 
 
 
