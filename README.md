09ccf3a92030c6718afb6e531134e7346536607a
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-08 15:31:02 -0400

    added wildcard

---
+++ README.md
@@ -0,0 +1,56 @@
+# Note Taker Web App
+
+![Accessibility](https://img.shields.io/badge/MIT-License-red)
+
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [MIT License](#MIT-License)
+* [Contributing](#Contributing)
+
+## Installation
+
+Run npm install for json packages then run node server.js and open port link on browser/ Use app: Open provided heroku link on browser and enjoy app.
+
+## Usage 
+Note taker website application where you can save all your activities 
+
+[Deployed website](https://gt-note-taker-express-server.herokuapp.com/)
+
+![Web App Upper ](./images/Background.png)
+![Web App Middle](./images/Optimization-Management.png)
+![Web App Lower ](./images/Media-Marketing.png)
+
+## Credits
+
+This was a single project completed as homework for Georgia Tech Coding Boot Camp /FullStack Flex Program.
+
+## MIT License
+
+Copyright (c) [2020] [Daniel Rojas]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
+
+## Contributing
+
+No contributions at this time. 
--- server.js
+++ server.js
@@ -27,115 +27,55 @@ app.get("/api/notes", (req, res) => {
    // read file and return notes object
     fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
         if (err) throw err;
-        // console.log("this is data", JSON.parse(data))
         return res.json(JSON.parse(data))
     })
   
 })
 
-    // < !-- * POST`/api/notes` - Should receive a new note to save on the request body, add it to the`db.json` file, and then return the new note to the client. -- >
-
-app.post("/api/notes", (req, res) => {
-
-    
+app.post("/api/notes", (req, res) => {   
   
     fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
         if (err) throw err;
         const arrayOfNotes = JSON.parse(data);
-        
-
-
-        console.log("this is my array",arrayOfNotes)
         const newNote = req.body
-        console.log("This is newNote", newNote);
         arrayOfNotes.push(newNote)
 
         //Add id to each obj and assign them the value of index
         for (let i = 0; i < arrayOfNotes.length; i++) {
             arrayOfNotes[i].id = i;
         }
-        console.log(arrayOfNotes);
-
-
-        
-    
-
-        const stringArray = JSON.stringify(arrayOfNotes)
-
-
-        
+        const stringArray = JSON.stringify(arrayOfNotes)       
 
         //Overwrite db file 
         fs.writeFile(__dirname + "/db/db.json", stringArray, (err, data) => {
             if (err) throw err;
-            console.log("this is my array after write",arrayOfNotes)
 
            return res.json(arrayOfNotes);
-        })
-
-        
-
-
-        
+        })       
     });
 });
-    
-
-
-//  * DELETE`/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.This means you'll need to find a way to give each note a unique `id` when it's saved.In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
 
 app.delete("/api/notes/:id", (req, res) => {
-
-
-
     const clickedID = parseInt(req.params.id)
-    console.log("this type is ", typeof clickedID)
-
-
     fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
         if (err) throw err;
         const arrayOfNotes = JSON.parse(data);
-
-        console.log("click delete buttons and array is", arrayOfNotes)
-
-        let filteredArrayOfNotes = arrayOfNotes.filter(function(objNotes) {
-     return objNotes.id !== clickedID
-            console.log("This is my obb", objNotes.id)
-        
+        let filteredArrayOfNotes = arrayOfNotes.filter(function (objNotes) {
+            return objNotes.id !== clickedID;        
         });
-        console.log("THIS IS A CLICKED NUMBER", clickedID)
-
-
-        console.log("this is my filterd array", filteredArrayOfNotes)
-
         fs.writeFile(__dirname + "/db/db.json", JSON.stringify(filteredArrayOfNotes), (err) => {
             if (err) throw err;
-
-            res.json(filteredArrayOfNotes)
+            res.json(filteredArrayOfNotes);
             
         });
-
-
-
-            
-
-
-
-
-    })
-
-
-
-    
+    })    
 });
 
-
+//Listen to PORT 
 app.listen(PORT, () => {
     console.log("Listening to http://localhost:" + PORT)
 })
 
-
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/index.html"));
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
